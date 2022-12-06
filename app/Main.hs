0331051806bb39004f65f2c7854d92898e1d9d74
Gunnar Lund <gunnar.l.n@gmail.com>
2022-12-05 17:46:38 -0800

    Add day4

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Day1 (answer)
 import qualified Day2 (easyAnswer, hardAnswer)
 import qualified Day3 (easyAnswer, hardAnswer)
+import qualified Day4 (easyAnswer, hardAnswer)
 
 main :: IO ()
 main = do
@@ -11,3 +12,5 @@ main = do
   Day2.hardAnswer
   Day3.easyAnswer
   Day3.hardAnswer
+  Day4.easyAnswer
+  Day4.hardAnswer
