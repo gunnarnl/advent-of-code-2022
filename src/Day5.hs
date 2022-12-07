e58504cd40338479b09cd09b01d4a960d6d3eacb
Gunnar Lund <gunnar.l.n@gmail.com>
2022-12-06 23:40:55 -0800

    Well the parser works

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,12 +1,19 @@
 module Day5 where
 import Data.List ( transpose )
 
-parseContents :: String -> ([String],[[String]])
+-- what a pain in the ass
+parseContents :: String -> ([String],[[Int]])
 parseContents = parseSides . break null . lines
   where
-    parseBox = filter (all (`elem` ['A'..'Z']++[' '])) . transpose . tail . reverse
-    parseRule = map (map last . takeWhile (not . null) . iterate (drop 2) . words) . tail
+    parseBox = filter (not . null) . map (takeWhile (`elem` ['A'..'Z'])) . transpose . tail . reverse
+    parseRule = map (reverse . map (read . last) . takeWhile (not . null) . iterate (init . init) . words) . tail
     parseSides (boxes, rules) = (parseBox boxes, parseRule rules)
+
+applyRule :: [String] -> [Int] -> [String]
+applyRule s (n:s1:s2) = removedList . splitAt s1
+  where move = take n . reverse
+        removedList (x,_:ys) = move (s !! s1)
+        removedList = 
 
 easyAnswer :: IO ()
 easyAnswer = do
